ddb6f4760378dcda48fad6ae01fbef372e019d52
Alvis Prieditis <reverbtail@gmail.com>
2024-03-03 05:52:28 -0500

    implemented  navbar dissapear functionality on scroll. Also worked on styling improvements.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Nav.css";
 import Logo from "./Logo.svg"
-import { useState } from 'react';
 import PersonIcon from '@mui/icons-material/Person';
 import ShoppingBasketRoundedIcon from '@mui/icons-material/ShoppingBasketRounded';
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  return (
-    <nav className="navbar">
-      <img src={Logo} alt="" />
-      <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">Reservations</a></li>
-        <li><a href="#"><PersonIcon fontSize='large'/></a></li>
-        <li><a href="#"><ShoppingBasketRoundedIcon fontSize='large'/></a></li>
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+    if (prevScrollPos > currentScrollPos) {
+      setIsScrolled(false);
+    } else {
+      setIsScrolled(true);
+    }
+    setPrevScrollPos(currentScrollPos);
+  };
 
-      </ul>
-      <div className="hamburger-menu" onClick={toggleMenu}>
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-      </div>
-    </nav>
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
+
+  return (
+      <nav className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
+        <img src={Logo} alt="" />
+        <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">Reservations</a></li>
+          <li><a href="#"><PersonIcon fontSize='large'/></a></li>
+          <li><a href="#"><ShoppingBasketRoundedIcon fontSize='large'/></a></li>
+        </ul>
+        <div className="hamburger-menu" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </nav>
   );
 };
 
