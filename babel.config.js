110a5a465ddcf828c7ac295e29b4ba13dcb65715
Alvis Prieditis <reverbtail@gmail.com>
2024-07-15 22:34:44 +0300

    configured babel and runned build

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
-export const presets = [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-react"
-];
-
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      ['@babel/preset-react', { runtime: 'automatic' }]
+    ],
+  };
+  
